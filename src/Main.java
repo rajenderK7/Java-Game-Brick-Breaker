ccc835a06724ba70e1bdf355291c6ba3a41fa834
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2022-02-02 14:06:18 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 		JFrame obj=new JFrame();
 		Gameplay gamePlay = new Gameplay();
 		
-		obj.setBounds(10, 10, 700, 600);
-		obj.setTitle("Breakout Ball");		
+		obj.setBounds(10, 10, 710, 610);
+		obj.setTitle("Brick Breaker");		
 		obj.setResizable(false);
 		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
